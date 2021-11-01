3ec5d6c5b663746ec84cc5120868b14b5a8dc436
yousoumar <oumaiyr@gmail.com>
2021-11-01 14:24:39 +0100

    adds search input

--- App.js
+++ App.js
@@ -9,11 +9,16 @@ import {
   ActivityIndicator,
   Image,
   SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar,
+  Button,
 } from "react-native";
 import * as Location from "expo-location";
 export default function App() {
   const [data, setData] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [inputValue, setInputValue] = useState("");
   useEffect(() => {
     setErrorMessage(null);
     setData(null);
@@ -34,7 +39,6 @@ export default function App() {
       );
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
         setData(data);
       } else {
         throw Error("Something went rong");
@@ -55,36 +59,65 @@ export default function App() {
 
   if (data) {
     return (
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Image
-            style={styles.tinyLogo}
-            source={{
-              uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`,
-            }}
-          />
-          <Text style={{ ...styles.text, color: "white" }}>
-            <Text style={{ fontSize: 100 }}>
-              {data && Math.trunc(data.main.temp)}
-            </Text>
-            °C
-          </Text>
-        </View>
-
-        <Text style={styles.text}>{data.weather[0].description}</Text>
-        <View
+      <>
+        <SafeAreaView
           style={{
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: 20,
+            backgroundColor: "#1e213a",
           }}
         >
-          <FontAwesomeIcon icon={faMapMarkerAlt} size={32} color={"white"} />
-          <Text style={{ ...styles.text, marginTop: 10, fontSize: 20 }}>
-            {data && data.name}
-          </Text>
-        </View>
-      </SafeAreaView>
+          <View
+            style={{
+              flexDirection: "row",
+              padding: 10,
+              justifyContent: "space-between",
+              marginTop: Platform.OS == "android" ? StatusBar.currentHeight : 0,
+            }}
+          >
+            <TextInput
+              style={styles.input}
+              placeholder="Entrer le nom d'une ville"
+              placeholderTextColor="#a09fb1"
+              value={inputValue}
+              onChangeText={(text) => setInputValue(text)}
+            />
+            <Button
+              title="Chercher"
+              style={styles.button}
+              onPress={console.log(inputValue)}
+            />
+          </View>
+        </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+          <View>
+            <Image
+              style={styles.tinyLogo}
+              source={{
+                uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`,
+              }}
+            />
+            <Text style={{ ...styles.text, color: "white" }}>
+              <Text style={{ fontSize: 100 }}>
+                {data && Math.trunc(data.main.temp)}
+              </Text>
+              °C
+            </Text>
+          </View>
+
+          <Text style={styles.text}>{data.weather[0].description}</Text>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+          >
+            <FontAwesomeIcon icon={faMapMarkerAlt} size={32} color={"white"} />
+            <Text style={{ ...styles.text, marginTop: 10, fontSize: 20 }}>
+              {data && data.name}
+            </Text>
+          </View>
+        </SafeAreaView>
+      </>
     );
   }
   return (
@@ -101,6 +134,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     color: "white",
+    position: "relative",
   },
   text: {
     color: "white",
@@ -111,5 +145,19 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 200,
     height: 100,
+  },
+  input: {
+    height: 40,
+    width: "70%",
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "white",
+    borderRadius: 10,
+    color: "white",
+  },
+  button: {
+    width: "25%",
+    height: "40",
+    borderRadius: 10,
   },
 });
